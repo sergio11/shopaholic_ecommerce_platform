55cbef417e4762287e83b63a8c8575cbbd292662
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-06-28 21:43:19 +0200

    Implement highly available MariaDB with Galera Cluster and HAProxy

--- mariadb_galera_cluster/apply_master_slave_configuration.sh
+++ mariadb_galera_cluster/apply_master_slave_configuration.sh
@@ -7,13 +7,13 @@ mysql -u replicant -preplicant00 -h mariadb_master_1 -e "FLUSH TABLES WITH READ 
 mysqldump -u replicant -preplicant00 -h mariadb_master_1 --all-databases --master-data --single-transaction --flush-logs --events > /tmp/master_dump.sql
 log_file=`mysql -u replicant -preplicant00 -h mariadb_master_1 -e "SHOW MASTER STATUS\G" | grep File: | awk '{print $2}'`
 pos=`mysql -u replicant -preplicant00 -h mariadb_master_1 -e "SHOW MASTER STATUS\G" | grep Position: | awk '{print $2}'`
-while ! mysqladmin ping -u iptv -piptv00 -h mariadb_slave_1 --silent; do
+while ! mysqladmin ping -u dreamsoftware -pdreamsoftware00 -h mariadb_slave_1 --silent; do
   sleep 1
 done
-mysql -u iptv -piptv00 -h mariadb_slave_1 -e "STOP SLAVE;";
-mysql -u iptv -piptv00 -h mariadb_slave_1 < /tmp/master_dump.sql
-mysql -u iptv -piptv00 -h mariadb_slave_1 -e "RESET SLAVE;";
-mysql -u iptv -piptv00 -h mariadb_slave_1 -e "CHANGE MASTER TO MASTER_HOST='mariadb_master_1', MASTER_USER='replicant', MASTER_PASSWORD='replicant00', MASTER_LOG_FILE='${log_file}', MASTER_LOG_POS=${pos};"
-mysql -u iptv -piptv00 -hmariadb_slave_1 -e "START SLAVE;"
+mysql -u dreamsoftware -pdreamsoftware00 -h mariadb_slave_1 -e "STOP SLAVE;";
+mysql -u dreamsoftware -pdreamsoftware00 -h mariadb_slave_1 < /tmp/master_dump.sql
+mysql -u dreamsoftware -pdreamsoftware00 -h mariadb_slave_1 -e "RESET SLAVE;";
+mysql -u dreamsoftware -pdreamsoftware00 -h mariadb_slave_1 -e "CHANGE MASTER TO MASTER_HOST='mariadb_master_1', MASTER_USER='replicant', MASTER_PASSWORD='replicant00', MASTER_LOG_FILE='${log_file}', MASTER_LOG_POS=${pos};"
+mysql -u dreamsoftware -pdreamsoftware00 -hmariadb_slave_1 -e "START SLAVE;"
 mysql -u replicant -preplicant00 -h mariadb_master_1 -e "UNLOCK TABLES;"
 echo "Master/Slave configuration finished" && sh /start_replicant_cluster_2.sh
--- mariadb_galera_cluster/mariadb_master_1/entrypoint-initdb.d/init.sql
+++ mariadb_galera_cluster/mariadb_master_1/entrypoint-initdb.d/init.sql
@@ -2,6 +2,6 @@ CREATE USER 'monitor'@'%' IDENTIFIED BY 'monitor';
 GRANT ALL PRIVILEGES on *.* TO 'monitor'@'%';
 CREATE USER 'replicant'@'%' IDENTIFIED BY 'replicant00';
 GRANT ALL PRIVILEGES on *.* TO 'replicant'@'%';
-CREATE USER 'iptv'@'%' IDENTIFIED BY 'iptv00';
-GRANT ALL PRIVILEGES on *.* TO 'iptv'@'%';
+CREATE USER 'dreamsoftware'@'%' IDENTIFIED BY 'dreamsoftware00';
+GRANT ALL PRIVILEGES on *.* TO 'dreamsoftware'@'%';
 FLUSH PRIVILEGES;
