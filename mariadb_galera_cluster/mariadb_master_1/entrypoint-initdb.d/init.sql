666637add37098652555c358d6084ea55c236150
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-06-29 21:56:55 +0200

    Implement highly available MariaDB with Galera Cluster and HAProxy

--- mariadb_galera_cluster/mariadb_master_1/entrypoint-initdb.d/init.sql
+++ mariadb_galera_cluster/mariadb_master_1/entrypoint-initdb.d/init.sql
@@ -1,5 +1,4 @@
-CREATE USER 'monitor'@'%' IDENTIFIED BY 'monitor';
-GRANT ALL PRIVILEGES on *.* TO 'monitor'@'%';
+CREATE USER 'monitor'@'%' IDENTIFIED BY 'monitor00';
 CREATE USER 'replicant'@'%' IDENTIFIED BY 'replicant00';
 GRANT ALL PRIVILEGES on *.* TO 'replicant'@'%';
 CREATE USER 'dreamsoftware'@'%' IDENTIFIED BY 'dreamsoftware00';
