2767348d8816dc1434ee749d71b3b1a7b8608035
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-07-01 15:36:42 +0200

    Implement highly available MariaDB with Galera Cluster and HAProxy

--- mariadb_galera_cluster/start_master_slave.sh
+++ mariadb_galera_cluster/start_master_slave.sh
@@ -4,5 +4,5 @@ sleep 60
 while ! mysqladmin ping -u replicant -preplicant00 -h mariadb_master_1 --silent; do
   sleep 1
 done
-exec ./configure_slave.sh &
-echo "MariaDB Cluster Galera Bootstrap master node available" && mysql_install_db --user=mysql --ldata=/var/lib/mysql/ && su - mysql -c mariadbd
+exec /usr/bin/configure_slave.sh &
+exit 0
--- mariadb_galera_cluster/start_slave.sh
+++ mariadb_galera_cluster/start_slave.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Waiting for MariaDB Cluster Galera Bootstrap master node to be accessable before starting replicant"
 sleep 60
-while ! mysqladmin ping -u replicant -preplicant00 -h mariadb_slave_1 --silent; do
+while ! mysqladmin ping -u monitor -pmonitor00 -h mariadb_slave_1 --silent; do
   sleep 1
 done
-exec ./configure_slave.sh &
-echo "MariaDB Cluster Galera Bootstrap master node available" && mysql_install_db --user=mysql --ldata=/var/lib/mysql/ && su - mysql -c mariadbd
+echo "Starting slave"
+exit 0
