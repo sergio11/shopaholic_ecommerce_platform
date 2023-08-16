e9de2b49eef920d46d4d8a6c0eb5d8f6b3a3bd98
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-08-16 21:19:44 +0200

    Configure API global prefix

--- platform/backend/src/main.ts
+++ platform/backend/src/main.ts
@@ -7,6 +7,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
   
   app.useGlobalPipes(
     new ValidationPipe({
