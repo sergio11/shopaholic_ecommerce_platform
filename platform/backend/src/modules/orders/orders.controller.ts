f4a7b59b1687264f95d1ae80573059520d5fd600
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-08-30 18:10:19 +0200

    Implement find and paginate orders

--- platform/backend/src/modules/orders/orders.controller.ts
+++ platform/backend/src/modules/orders/orders.controller.ts
@@ -1,32 +1,52 @@
-import { Delete, Param, Get, Version } from '@nestjs/common';
+import { Delete, Param, Get, Version, DefaultValuePipe, ParseIntPipe, Query } from '@nestjs/common';
 import { JwtRole } from '../auth/jwt/jwt-role';
 import { OrdersService } from './orders.service';
-import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiQuery, ApiResponse } from '@nestjs/swagger';
 import { OrderResponseDto } from './dto/order-response.dto';
 import { Auth } from '../auth/decorator/auth.decorator';
 import { ApiController } from 'src/core/decorator/default-api.decorator';
 import { AuthUserId } from '../auth/decorator/auth-user-id.decorator';
+import { Pagination } from 'nestjs-typeorm-paginate';
 
 @ApiController('orders')
 export class OrdersController {
   constructor(private ordersService: OrdersService) {}
 
-  /**
-   * Retrieve a list of all orders.
-   * @returns {Promise<OrderResponseDto[]>} List of orders.
+   /**
+   * Retrieve orders for any client with pagination.
+   * @param {number} page - The page number for pagination (default is 1).
+   * @param {number} limit - The number of items per page (default is 10).
+   * @returns {Promise<Pagination<OrderResponseDto>>} Paginated list of orders.
    */
-  @Auth(JwtRole.ADMIN)
-  @Version('1.0')
-  @Get()
-  @ApiOperation({ summary: 'Retrieve a list of all orders' })
-  @ApiResponse({
-    status: 200,
-    description: 'List of orders',
-    type: [OrderResponseDto],
-  })
-  async findAll(): Promise<OrderResponseDto[]> {
-    return this.ordersService.findAll();
-  }
+   @Auth(JwtRole.ADMIN)
+   @Version('1.0')
+   @Get('all')
+   @ApiOperation({ summary: 'Retrieve orders for any client with pagination' })
+   @ApiQuery({
+     name: 'page',
+     required: false,
+     description: 'Page number (default is 1)',
+     example: 1,
+     type: Number,
+   })
+   @ApiQuery({
+     name: 'limit',
+     required: false,
+     description: 'Items per page (default is 10)',
+     example: 10,
+     type: Number,
+   })
+   @ApiResponse({
+     status: 200,
+     description: 'Paginated list of orders',
+     type: Pagination<OrderResponseDto>,
+   })
+   async findOrdersForAnyClientWithPagination(
+     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number = 1,
+     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number = 10,
+   ): Promise<Pagination<OrderResponseDto>> {
+     return this.ordersService.findOrdersForAnyClientWithPagination(page, limit);
+   }
 
   /**
    * Retrieve orders by client ID.
