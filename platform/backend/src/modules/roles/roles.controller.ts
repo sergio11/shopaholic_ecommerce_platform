1a2ed45502ed1ae739c9dc64936f56cc858ecb7a
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-09-01 17:15:50 +0200

    Implement endpoint to get all roles

--- platform/backend/src/modules/roles/roles.controller.ts
+++ platform/backend/src/modules/roles/roles.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Delete, Param, Post, UploadedFile, Version } from '@nestjs/common';
+import {
+  Body,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  Version,
+} from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
@@ -9,63 +17,78 @@ import { DefaultUploadFileValidationDecorator } from 'src/core/decorator/default
 import { Auth } from '../auth/decorator/auth.decorator';
 import { ApiController } from 'src/core/decorator/default-api.decorator';
 
-
 @ApiController('roles')
 export class RolesController {
+  constructor(private rolesService: RolesService) {}
 
-    constructor(private rolesService: RolesService) {}
+  /**
+   * Retrieves a list of all roles.
+   * @returns A list of roles.
+   */
+  @Auth(JwtRole.ADMIN)
+  @Version('1.0')
+  @Get('all')
+  @ApiOperation({ summary: 'Get a list of all roles' }) // Add an operation summary
+  @ApiResponse({
+    status: 200,
+    description: 'List of roles retrieved successfully',
+    type: [RoleResponseDto], // Specify the response type
+  })
+  async listRoles(): Promise<RoleResponseDto[]> {
+    return await this.rolesService.findAll();
+  }
 
-    /**
-     * Creates a new role with the given information and image file.
-     * @param file - The image file associated with the role.
-     * @param role - The data to create the new role.
-     * @returns The newly created role.
-     */
-    @Auth(JwtRole.ADMIN)
-    @Version('1.0')
-    @Post()
-    @DefaultUploadFileValidationDecorator()
-    @ApiOperation({ summary: 'Create new role' })
-    @ApiResponse({ status: 403, description: 'Forbidden.' })
-    async create(
-        @UploadedFile() file: Express.Multer.File,
-        @Body() roleData: CreateRoleDto
-    ): Promise<RoleResponseDto> {
-        const role = { ...roleData, imageFile: file};
-        return this.rolesService.create(role);
-    }
+  /**
+   * Creates a new role with the given information and image file.
+   * @param file - The image file associated with the role.
+   * @param role - The data to create the new role.
+   * @returns The newly created role.
+   */
+  @Auth(JwtRole.ADMIN)
+  @Version('1.0')
+  @Post()
+  @DefaultUploadFileValidationDecorator()
+  @ApiOperation({ summary: 'Create new role' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  async create(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() roleData: CreateRoleDto,
+  ): Promise<RoleResponseDto> {
+    const role = { ...roleData, imageFile: file };
+    return this.rolesService.create(role);
+  }
 
-    /**
-     * Updates an existing role with the given information and image file.
-     * @param file - The new image file associated with the role.
-     * @param id - The ID of the role to update.
-     * @param role - The updated data for the role.
-     * @returns The updated role.
-     */
-    @Auth(JwtRole.ADMIN)
-    @Version('1.0')
-    @Post(':id')
-    @DefaultUploadFileValidationDecorator({ isOptional: true })
-    @ApiOperation({ summary: 'Update role by ID' })
-    @ApiResponse({ status: 403, description: 'Forbidden.' })
-    async update(
-        @UploadedFile() file: Express.Multer.File,
-        @Param('id') id: string, 
-        @Body() roleData: UpdateRoleDto
-    ): Promise<RoleResponseDto> {
-        const role = { ...roleData, imageFile: file};
-        return this.rolesService.update(id, role);
-    }
+  /**
+   * Updates an existing role with the given information and image file.
+   * @param file - The new image file associated with the role.
+   * @param id - The ID of the role to update.
+   * @param role - The updated data for the role.
+   * @returns The updated role.
+   */
+  @Auth(JwtRole.ADMIN)
+  @Version('1.0')
+  @Post(':id')
+  @DefaultUploadFileValidationDecorator({ isOptional: true })
+  @ApiOperation({ summary: 'Update role by ID' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  async update(
+    @UploadedFile() file: Express.Multer.File,
+    @Param('id') id: string,
+    @Body() roleData: UpdateRoleDto,
+  ): Promise<RoleResponseDto> {
+    const role = { ...roleData, imageFile: file };
+    return this.rolesService.update(id, role);
+  }
 
-    /**
-     * Deletes a role with the specified ID.
-     * @param id - The ID of the role to delete.
-     */
-    @Auth(JwtRole.ADMIN)
-    @Version('1.0')
-    @Delete(':id')
-    @ApiOperation({ summary: 'Delete role by ID' })
-    async delete(@Param('id') id: string): Promise<string> {
-        return this.rolesService.delete(id);
-    }
+  /**
+   * Deletes a role with the specified ID.
+   * @param id - The ID of the role to delete.
+   */
+  @Auth(JwtRole.ADMIN)
+  @Version('1.0')
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete role by ID' })
+  async delete(@Param('id') id: string): Promise<string> {
+    return this.rolesService.delete(id);
+  }
 }
