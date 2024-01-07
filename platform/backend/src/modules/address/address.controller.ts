c359b7ea8630cb807eeb2ab059a7b3dc6e5850ff
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2024-01-07 12:47:41 +0100

    Implement several improvements at AddressController

--- platform/backend/src/modules/address/address.controller.ts
+++ platform/backend/src/modules/address/address.controller.ts
@@ -7,34 +7,39 @@ import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AddressResponseDto } from './dto/address-response.dto';
 import { Auth } from '../auth/decorator/auth.decorator';
 import { ApiController } from 'src/core/decorator/default-api.decorator';
+import { AuthUserId } from '../auth/decorator/auth-user-id.decorator';
 
 @ApiController('address')
 export class AddressController {
   constructor(private addressService: AddressService) {}
 
   /**
-   * Create a new address.
+   * Create a new address for the currently authenticated user.
    * @param {CreateAddressDto} address - The address data to create.
+   * @param {string} userId - ID of the authenticated user.
    * @returns {Promise<AddressResponseDto>} The newly created address.
    */
   @Auth(JwtRole.ADMIN, JwtRole.CLIENT)
   @Version('1.0')
   @Post()
-  @ApiOperation({ summary: 'Create a new address' })
+  @ApiOperation({ summary: 'Create a new address for the currently authenticated user.' })
   @ApiResponse({
     status: 200,
     description: 'A new address created',
     type: AddressResponseDto,
   })
-  async create(@Body() address: CreateAddressDto): Promise<AddressResponseDto> {
-    return this.addressService.create(address);
+  async create(
+    @Body() address: CreateAddressDto,
+    @AuthUserId() userId: string,
+  ): Promise<AddressResponseDto> {
+    return this.addressService.create(address, userId);
   }
 
   /**
    * Return all addresses that have been registered.
    * @returns {Promise<AddressResponseDto[]>} A list of registered addresses.
    */
-  @Auth(JwtRole.ADMIN, JwtRole.CLIENT)
+  @Auth(JwtRole.ADMIN)
   @Version('1.0')
   @Get()
   @ApiOperation({ summary: 'Return all addresses that have been registered' })
@@ -48,11 +53,31 @@ export class AddressController {
   }
 
   /**
+   * Return all addresses of the currently authenticated user.
+   * @returns {Promise<AddressResponseDto[]>} A list of addresses of the authenticated user.
+   */
+  @Auth(JwtRole.ADMIN, JwtRole.CLIENT)
+  @Version('1.0')
+  @Get('user/')
+  @ApiOperation({ summary: 'Return all addresses of the currently authenticated user.' })
+  @ApiResponse({
+    status: 200,
+    description: 'All addresses of the currently authenticated user',
+    type: AddressResponseDto,
+  })
+  async findAllCurrentUserAddresses(
+    @AuthUserId() userId: string,
+  ): Promise<AddressResponseDto[]> {
+    console.log(`findAllCurrentUserAddresses - userId: ${userId}`)
+    return this.addressService.findByUser(userId);
+  }
+
+  /**
    * Return all addresses that have been registered by a user.
    * @param {string} id_user - The user ID associated with the addresses.
    * @returns {Promise<AddressResponseDto[]>} A list of addresses registered by the user.
    */
-  @Auth(JwtRole.ADMIN, JwtRole.CLIENT)
+  @Auth(JwtRole.ADMIN)
   @Version('1.0')
   @Get('user/:id_user')
   @ApiOperation({
@@ -73,6 +98,7 @@ export class AddressController {
    * Update the data of an address.
    * @param {string} id - The ID of the address to update.
    * @param {UpdateAddressDto} address - The updated address data.
+   * @param {string} userId - User authenticated id.
    * @returns {Promise<AddressResponseDto>} The updated address.
    */
   @Auth(JwtRole.ADMIN, JwtRole.CLIENT)
@@ -87,13 +113,15 @@ export class AddressController {
   async update(
     @Param('id') id: string,
     @Body() address: UpdateAddressDto,
+    @AuthUserId() userId: string,
   ): Promise<AddressResponseDto> {
-    return this.addressService.update(id, address);
+    return this.addressService.update(id, address, userId);
   }
 
   /**
    * Delete an address.
    * @param {string} id - The ID of the address to delete.
+   * @param {string} userId - User authenticated id.
    * @returns {Promise<void>} The deleted address.
    */
   @Auth(JwtRole.ADMIN, JwtRole.CLIENT)
@@ -104,7 +132,10 @@ export class AddressController {
     status: 200,
     description: 'The address deleted by the user',
   })
-  async delete(@Param('id') id: string): Promise<string> {
-    return this.addressService.delete(id);
+  async delete(
+    @Param('id') id: string,
+    @AuthUserId() userId: string
+  ): Promise<string> {
+    return this.addressService.delete(id, userId);
   }
 }
